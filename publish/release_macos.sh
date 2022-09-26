58b9f5d7cf8eaa9f877fa4e26ed74be2c6d05271
chenyong <chenyong@star-net.cn>
2022-09-26 09:30:37 +0800

    修复mac打包后无法运行

--- publish/release_macos.sh
+++ publish/release_macos.sh
@@ -16,9 +16,8 @@ make -j
 mv app.app loginsight.app
 cp ../../../core/build/macosx/x86_64/release/core.macosx loginsight.app/Contents/MacOS/
 mv loginsight.app/Contents/MacOS/app loginsight.app/Contents/MacOS/loginsight 
-macdeployqt loginsight.app -qmldir=../../../app
-cp -a ~/Qt/5.15.2/clang_64/qml/QtQuick/Controls/Styles/Desktop loginsight.app/Contents/Resources/qml/QtQuick/Controls/Styles
-rm -rf release/loginsight.app
+macdeployqt loginsight.app -no-strip -always-overwrite -qmldir=../../../app
+rm -rf ../../release/loginsight.app
 mv loginsight.app ../../release/loginsight.app
 cd ../../
 rm -rf build/macx
