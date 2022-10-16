0342dbbdc0caa811972c4466d4c46b0a5bf474b1
chenyong <chenyong@star-net.cn>
2022-10-16 14:12:05 +0800

    mac魔数问题

--- publish/pack.sh
+++ publish/pack.sh
@@ -1,5 +1,4 @@
 cd release
-zip -r macosx.zip loginsight.app
 
 mv windows loginsight
 zip -r windows.zip loginsight
--- publish/release_macos.sh
+++ publish/release_macos.sh
@@ -16,8 +16,11 @@ make -j
 mv app.app loginsight.app
 cp ../../../core/build/macosx/x86_64/release/core.macosx loginsight.app/Contents/MacOS/
 mv loginsight.app/Contents/MacOS/app loginsight.app/Contents/MacOS/loginsight 
-macdeployqt loginsight.app -no-strip -always-overwrite -qmldir=../../../app
+macdeployqt loginsight.app -no-strip -qmldir=../../../app
 rm -rf ../../release/loginsight.app
 mv loginsight.app ../../release/loginsight.app
 cd ../../
+cd release
+zip -r macosx.zip loginsight.app # 不能在pack里压缩，因为macos下的软连接在linux下并不是被识别为软连接，压缩后还原给macos就会变成普通文件，出现magic word
+cd -
 rm -rf build/macx
